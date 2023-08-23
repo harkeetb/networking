5b8fed6b1e15d876993656d1974e6343c7f8b833
harkeetb <93454413+harkeetb@users.noreply.github.com>
2023-08-22 22:03:22 -0500

    Initialized client socket object and defined functions and constants

--- terminal_chatroom/client.py
+++ terminal_chatroom/client.py
@@ -1,3 +1,25 @@
 #Harkeet Bal
 #Client-side chat room
 
+import socket
+import threading
+
+#define constants
+DEST_IP = socket.gethostbyname(socket.gethostname())
+DEST_PORT = 12345
+ENCODER = 'utf-8'
+BYTE_SIZE = 1024
+
+#initialize client socket object, and connect to destination address
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect((DEST_IP, DEST_PORT))
+
+#function: sendMessage
+#purpose: send a message to the server which will be broadcasted
+def sendMessage():
+    pass
+
+#function receiveMessage
+#purpose: receive an incoming message via server
+def receiveMessage():
+    pass
