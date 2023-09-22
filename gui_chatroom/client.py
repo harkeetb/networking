db0ca7655b27d54663408fec5939d030d746ad2c
harkeetb <93454413+harkeetb@users.noreply.github.com>
2023-09-22 18:36:30 -0500

    declared client-side functions

--- gui_chatroom/client.py
+++ gui_chatroom/client.py
@@ -8,6 +8,32 @@ import threading
 #import the gui defined in gui.py
 root = gui.root
 
+#define functions:
+
+#function: connect
+#purpose: connect to a server at it's given IP/Port address
+def connect():
+    pass
+
+#function: verify
+#purpose: verify that the server connection is valid
+def verify():
+    pass
+
+#function: disconnect
+#purpose: disconnect from the chat server
+def disconnect():
+    pass
+
+#function: send
+#purpose send a message to the chat server
+def send():
+    pass
+
+#function: receive
+#purpose: send receive a message from the chat server
+def receive():
+    pass
 
 
 #run root window
