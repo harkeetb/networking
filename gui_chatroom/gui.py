7b4ca89b9186ed7e244b019937b4d8cf709bf1e2
harkeetb <93454413+harkeetb@users.noreply.github.com>
2023-09-22 15:40:28 -0500

    Initial commit

---
+++ gui_chatroom/gui.py
@@ -0,0 +1,22 @@
+import tkinter
+
+#define root window
+root = tkinter.Tk()
+root.title("Chatroom")
+root.geometry('400x600')
+root.resizable(0,0)         #disable window resizing
+
+#define colours
+root_color = '#535657'
+input_color = '#4f646f'
+output_color = "#dee7e7"
+root.config(bg=root_color)
+
+#define GUI layout
+
+
+#define functions
+
+
+#root window main loop
+root.mainloop()
