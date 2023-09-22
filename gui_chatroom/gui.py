9510f0df0ec6f892aef8ee4b4f3448a6f4a36a53
harkeetb <93454413+harkeetb@users.noreply.github.com>
2023-09-22 16:26:08 -0500

    implemented send functionality and button

--- gui_chatroom/gui.py
+++ gui_chatroom/gui.py
@@ -1,5 +1,5 @@
 import tkinter
-from tkinter import BOTH
+from tkinter import BOTH, END
 
 #define root window
 root = tkinter.Tk()
@@ -21,15 +21,21 @@ output_frame = tkinter.LabelFrame(root, bg=output_color)
 input_frame.pack(pady=10)
 output_frame.pack(padx=10, pady=(0,10), fill=BOTH, expand=True)  #fill output frame both vertically and horizontally
 
+#define functions:
+
+#send user's typed message to the output frame
+def send():
+    message_label = tkinter.Label(output_frame, text=message_entry.get(), fg='#000', bg=output_color, font=("Helvetica", 12))
+    message_label.pack()
+
+    #clear entry field
+    message_entry.delete(0,END)
+
 #define widgets
 message_entry = tkinter.Entry(input_frame, text="Enter message", width=20)
-send_button = tkinter.Button(input_frame, text="Send")                      
+send_button = tkinter.Button(input_frame, text="Send", command=send)                      
 message_entry.grid(row=0, column=0, padx=10, pady=10)                   
 send_button.grid(row=0, column=1, padx=10, pady=10, ipadx=10, ipady=5)
-
-
-#define functions
-
 
 #root window main loop
 root.mainloop()
